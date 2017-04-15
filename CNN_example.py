fde3cf4e56a9b27b7650ebf4fc73c969db3175c7
eiffiy <eiffiy@hotmail.com>
2017-04-15 16:22:11 +0100

    modify

--- CNN_example.py
+++ CNN_example.py
@@ -14,6 +14,10 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+import os
+from PIL import Image
+import numpy as np
+
 
 def label_reader(file_string):
     file = file_string
@@ -32,8 +36,6 @@ def label_reader(file_string):
         label = 5
     return label
 
-#读取文件夹mnist下的42000张图片，图片为灰度图，所以为1通道，
-#如果是将彩色图作为输入,则将1替换为3，并且data[i,:,:,:] = arr改为data[i,:,:,:] = [arr[:,:,0],arr[:,:,1],arr[:,:,2]]
 def load_data():
     data = np.empty((213,1,256,256),dtype="float32")
     label = np.empty((213,),dtype="uint8")
@@ -69,8 +71,8 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=x_train.shape[1:]))
+model.add(Conv2D(32, kernel_size = 4, activation='relu', strides = 1, input_shape=(1,256,256), data_format='channels_first'))
+
 model.add(Activation('relu'))
 model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
